ed3ed25182f5a8e3940afdc6ed83e57c4f6d7a11
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-16 22:21:53 +0100

    All cells die in a board with a single cell

--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -5,22 +5,34 @@ import org.junit.Test
 class GameOfLifeTest {
 
     @Test fun `empty board stays empty in the next generation`() {
-        assertThat(Board().nextGeneration(), IsEqual(Board()))
+        assertThat(Board(emptyList()).nextGeneration(), IsEqual(Board(emptyList())))
+    }
+
+    @Test fun `all cells die in a board with a single cell`() {
+        assertThat(Board(listOf(Cell())).nextGeneration(), IsEqual(Board(emptyList())))
     }
 }
 
-class Board {
+class Cell {
+    override fun toString(): String {
+        return "Cell()"
+    }
+}
+
+class Board(private val cells: List<Cell>) {
+
     fun nextGeneration(): Board {
-        return Board()
+        return Board(emptyList())
     }
 
     override fun toString(): String {
-        return "Board()"
+        return "Board($cells)"
     }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        return javaClass == other?.javaClass
+        if (other !is Board) return false
+        return other.cells == cells
     }
 
     override fun hashCode(): Int {
