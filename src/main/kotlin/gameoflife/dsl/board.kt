fa54a2f29f37176d46745cc14661549f796e0926
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-17 17:04:03 +0100

    Convert board DSL to an expression body

--- src/main/kotlin/gameoflife/dsl/board.kt
+++ src/main/kotlin/gameoflife/dsl/board.kt
@@ -2,6 +2,4 @@ package gameoflife.dsl
 
 import gameoflife.Board
 
-fun board(grid: (BoardBuilder.() -> Unit)): Board {
-    return BoardBuilder(grid).build()
-}
+fun board(grid: (BoardBuilder.() -> Unit)): Board = BoardBuilder(grid).build()
