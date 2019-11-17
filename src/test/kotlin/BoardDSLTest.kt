706f10293fbdbc5199c98763aa9e9c2a6d41fbee
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-17 17:09:48 +0100

    Import assertThat directly in the board DSL tests

--- src/test/kotlin/BoardDSLTest.kt
+++ src/test/kotlin/BoardDSLTest.kt
@@ -2,19 +2,19 @@ import gameoflife.Board
 import gameoflife.Cell
 import gameoflife.dsl.board
 import org.hamcrest.core.IsEqual
-import org.junit.Assert
+import org.junit.Assert.assertThat
 import org.junit.Test
 
 class BoardDSLTest {
 
     @Test fun `creates an empty board`() {
-        Assert.assertThat(board {}, IsEqual(Board()))
+        assertThat(board {}, IsEqual(Board()))
     }
 
     @Test fun `creates a board with cells`() {
-        Assert.assertThat(
+        assertThat(
             board { O; X; O },
-           IsEqual(Board(Cell(0).alive(), Cell(1).dead(), Cell(2).alive()))
+            IsEqual(Board(Cell(0).alive(), Cell(1).dead(), Cell(2).alive()))
         )
     }
 }
