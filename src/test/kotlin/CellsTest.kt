620d8c6dcf07e315c0c600172cf533e51a82104a
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-17 22:01:50 +0100

    Pass row to each cell in the Cells tests

--- src/test/kotlin/CellsTest.kt
+++ src/test/kotlin/CellsTest.kt
@@ -10,10 +10,10 @@ class CellsTest {
     @Test fun `equal if has the some cells as the other one`() {
         assertThat(
             Cells(listOf(
-                Cell(1).alive(), Cell(2).dead(), Cell(4).alive()
+                Cell(1, 2).alive(), Cell(2, 3).dead(), Cell(4, 4).alive()
             )),
             IsEqual(Cells(listOf(
-                Cell(1).alive(), Cell(2).dead(), Cell(4).alive()
+                Cell(1, 2).alive(), Cell(2, 3).dead(), Cell(4, 4).alive()
             )))
         )
     }
@@ -21,10 +21,10 @@ class CellsTest {
     @Test fun `unequal if other cells are in a different state`() {
         assertThat(
             Cells(listOf(
-                Cell(1).alive(), Cell(2).dead(), Cell(4).alive()
+                Cell(1, 2).alive(), Cell(2, 3).dead(), Cell(4, 4).alive()
             )),
             not(IsEqual(Cells(listOf(
-                Cell(1).alive(), Cell(2).alive(), Cell(4).dead()
+                Cell(1, 2).alive(), Cell(2, 3).alive(), Cell(4, 4).dead()
             ))))
         )
     }
@@ -32,12 +32,12 @@ class CellsTest {
     @Test fun `finds cells only existing in other cells`() {
         assertThat(
             Cells(listOf(
-                Cell(1), Cell(2), Cell(3), Cell(4), Cell(5).alive()
+                Cell(1, 2), Cell(2, 2), Cell(3, 2), Cell(4, 2), Cell(5, 2).alive(), Cell(6, 2)
             )).onlyExistingIn(Cells(listOf(
-                Cell(2), Cell(4), Cell(5).dead()
+                Cell(2, 2), Cell(4, 2), Cell(5, 2).dead(), Cell(6, 3)
             ))),
             IsEqual(Cells(listOf(
-                Cell(2), Cell(4)
+                Cell(2, 2), Cell(4, 2)
             )))
         )
     }
@@ -45,10 +45,10 @@ class CellsTest {
     @Test fun `maps each of the cells with given transformation`() {
         assertThat(
             Cells(listOf(
-                Cell(1).alive(), Cell(2).alive(), Cell(3).alive()
+                Cell(1, 2).alive(), Cell(2, 2).alive(), Cell(3, 2).alive()
             )).map { it.dead() },
             IsEqual(Cells(listOf(
-                Cell(1).dead(), Cell(2).dead(), Cell(3).dead()
+                Cell(1, 2).dead(), Cell(2, 2).dead(), Cell(3, 2).dead()
             )))
         )
     }
