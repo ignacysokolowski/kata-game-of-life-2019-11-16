46d28c8dc904e5d3142ae341b90234b9e005f855
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-17 21:58:13 +0100

    Add row to cells
    Defaults to 0 for now, before all the other code is migrated.

--- src/main/kotlin/gameoflife/Cell.kt
+++ src/main/kotlin/gameoflife/Cell.kt
@@ -1,10 +1,10 @@
 package gameoflife
 
-class Cell(private val column: Int, private val alive: Boolean = true) {
+class Cell(private val column: Int, private val row: Int = 0, private val alive: Boolean = true) {
 
-    fun alive() = Cell(column, alive = true)
+    fun alive() = Cell(column, row, alive = true)
 
-    fun dead() = Cell(column, alive = false)
+    fun dead() = Cell(column, row, alive = false)
 
     fun nextGenerationGivenNeighbours(neighbours: Int): Cell {
         return if (neighbours == 2) alive() else dead()
@@ -12,16 +12,16 @@ class Cell(private val column: Int, private val alive: Boolean = true) {
 
     fun neighbours() = listOf(leftNeighbour(), rightNeighbour())
 
-    private fun leftNeighbour() = Cell(column - 1)
+    private fun leftNeighbour() = Cell(column - 1, row)
 
-    private fun rightNeighbour() = Cell(column + 1)
+    private fun rightNeighbour() = Cell(column + 1, row)
 
-    override fun toString() = "Cell($column, ${if (alive) "alive" else "dead"})"
+    override fun toString() = "Cell($column, $row, ${if (alive) "alive" else "dead"})"
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Cell) return false
-        return other.column == column && other.alive == alive
+        return other.column == column && other.row == row && other.alive == alive
     }
 
     override fun hashCode() = javaClass.hashCode()
--- src/test/kotlin/CellTest.kt
+++ src/test/kotlin/CellTest.kt
@@ -7,47 +7,55 @@ import org.junit.Test
 class CellTest {
 
     @Test fun `is alive by default`() {
-        assertThat(Cell(2), IsEqual(Cell(2).alive()))
+        assertThat(Cell(2, 1), IsEqual(Cell(2, 1).alive()))
     }
 
     @Test fun `is not dead by default`() {
-        assertThat(Cell(2), not(IsEqual(Cell(2).dead())))
+        assertThat(Cell(2, 1), not(IsEqual(Cell(2, 1).dead())))
     }
 
     @Test fun `can come to live`() {
-        assertThat(Cell(2).dead().alive(), IsEqual(Cell(2).alive()))
+        assertThat(Cell(2, 1).dead().alive(), IsEqual(Cell(2, 1).alive()))
     }
 
     @Test fun `two equal alive cells`() {
-        assertThat(Cell(2).alive(), IsEqual(Cell(2).alive()))
+        assertThat(Cell(2, 1).alive(), IsEqual(Cell(2, 1).alive()))
     }
 
     @Test fun `two equal dead cells`() {
-        assertThat(Cell(2).dead(), IsEqual(Cell(2).dead()))
+        assertThat(Cell(2, 1).dead(), IsEqual(Cell(2, 1).dead()))
     }
 
     @Test fun `two unequal alive cells`() {
-        assertThat(Cell(2).alive(), not(IsEqual(Cell(3).alive())))
+        assertThat(Cell(2, 1).alive(), not(IsEqual(Cell(3, 1).alive())))
+    }
+
+    @Test fun `two alive cells with unequal row`() {
+        assertThat(Cell(2, 1).alive(), not(IsEqual(Cell(2, 2).alive())))
     }
 
     @Test fun `two unequal dead cells`() {
-        assertThat(Cell(2).dead(), not(IsEqual(Cell(3).dead())))
+        assertThat(Cell(2, 1).dead(), not(IsEqual(Cell(3, 1).dead())))
+    }
+
+    @Test fun `two dead cells with unequal row`() {
+        assertThat(Cell(2, 1).dead(), not(IsEqual(Cell(2, 2).dead())))
     }
 
     @Test fun `has neighbours on the left and right`() {
-        assertThat(Cell(1).neighbours(), IsEqual(listOf(Cell(0), Cell(2))))
-        assertThat(Cell(2).neighbours(), IsEqual(listOf(Cell(1), Cell(3))))
+        assertThat(Cell(1, 1).neighbours(), IsEqual(listOf(Cell(0, 1), Cell(2, 1))))
+        assertThat(Cell(2, 2).neighbours(), IsEqual(listOf(Cell(1, 2), Cell(3, 2))))
     }
 
     @Test fun `dies without neighbours`() {
-        assertThat(Cell(1).alive().nextGenerationGivenNeighbours(0), IsEqual(Cell(1).dead()))
+        assertThat(Cell(1, 2).alive().nextGenerationGivenNeighbours(0), IsEqual(Cell(1, 2).dead()))
     }
 
     @Test fun `dies with one neighbour`() {
-        assertThat(Cell(1).alive().nextGenerationGivenNeighbours(1), IsEqual(Cell(1).dead()))
+        assertThat(Cell(1, 2).alive().nextGenerationGivenNeighbours(1), IsEqual(Cell(1, 2).dead()))
     }
 
     @Test fun `survives with two neighbours`() {
-        assertThat(Cell(1).alive().nextGenerationGivenNeighbours(2), IsEqual(Cell(1).alive()))
+        assertThat(Cell(1, 2).alive().nextGenerationGivenNeighbours(2), IsEqual(Cell(1, 2).alive()))
     }
 }
