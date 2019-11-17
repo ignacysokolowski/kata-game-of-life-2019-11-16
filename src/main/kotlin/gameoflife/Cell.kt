c003219437047e348f90cc827100473c9eb45e35
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-17 17:02:44 +0100

    Convert Cell methods to expression bodies

--- src/main/kotlin/gameoflife/Cell.kt
+++ src/main/kotlin/gameoflife/Cell.kt
@@ -1,9 +1,7 @@
 package gameoflife
 
 class Cell(private val row: Int, private val alive: Boolean = true) {
-    override fun toString(): String {
-        return "Cell($row, ${if (alive) "alive" else "dead"})"
-    }
+    override fun toString() = "Cell($row, ${if (alive) "alive" else "dead"})"
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -11,27 +9,15 @@ class Cell(private val row: Int, private val alive: Boolean = true) {
         return other.row == row && other.alive == alive
     }
 
-    override fun hashCode(): Int {
-        return javaClass.hashCode()
-    }
+    override fun hashCode() = javaClass.hashCode()
 
-    fun alive(): Cell {
-        return Cell(row, alive = true)
-    }
+    fun alive() = Cell(row, alive = true)
 
-    fun dead(): Cell {
-        return Cell(row, alive = false)
-    }
+    fun dead() = Cell(row, alive = false)
 
-    fun neighbours(): Set<Cell> {
-        return setOf(leftNeighbour(), rightNeighbour())
-    }
+    fun neighbours() = setOf(leftNeighbour(), rightNeighbour())
 
-    private fun leftNeighbour(): Cell {
-        return Cell(row - 1)
-    }
+    private fun leftNeighbour() = Cell(row - 1)
 
-    private fun rightNeighbour(): Cell {
-        return Cell(row + 1)
-    }
+    private fun rightNeighbour() = Cell(row + 1)
 }
