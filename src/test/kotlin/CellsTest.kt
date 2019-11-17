fa7011300e70248eda8abbe5b2518d1a3f8c532f
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-17 18:03:20 +0100

    Add tests for cells equality
    If this equality stops working, most of the other tests are useless
    because they depend on it.

--- src/test/kotlin/CellsTest.kt
+++ src/test/kotlin/CellsTest.kt
@@ -1,10 +1,33 @@
 import gameoflife.Cell
 import gameoflife.Cells
 import org.hamcrest.core.IsEqual
+import org.hamcrest.core.IsNot.not
 import org.junit.Test
 import org.junit.Assert.assertThat
 
 class CellsTest {
+
+    @Test fun `equal if has the some cells as the other one`() {
+        assertThat(
+            Cells(listOf(
+                Cell(1).alive(), Cell(2).dead(), Cell(4).alive()
+            )),
+            IsEqual(Cells(listOf(
+                Cell(1).alive(), Cell(2).dead(), Cell(4).alive()
+            )))
+        )
+    }
+
+    @Test fun `unequal if other cells are in a different state`() {
+        assertThat(
+            Cells(listOf(
+                Cell(1).alive(), Cell(2).dead(), Cell(4).alive()
+            )),
+            not(IsEqual(Cells(listOf(
+                Cell(1).alive(), Cell(2).alive(), Cell(4).dead()
+            ))))
+        )
+    }
 
     @Test fun `finds cells only existing in other cells`() {
         assertThat(
