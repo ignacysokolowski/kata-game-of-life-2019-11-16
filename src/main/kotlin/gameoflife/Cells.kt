107357073b0dc55150edf57421c1e87e338fe81e
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-24 17:57:27 +0100

    Remove redundant 'constructor' keyword

--- src/main/kotlin/gameoflife/Cells.kt
+++ src/main/kotlin/gameoflife/Cells.kt
@@ -1,6 +1,6 @@
 package gameoflife
 
-class Cells constructor(private val cells: Set<Cell>) {
+class Cells(private val cells: Set<Cell>) {
     constructor(vararg cells: Cell) : this(cells.toSet())
     private constructor(cells: List<Cell>) : this(cells.toSet())
 
